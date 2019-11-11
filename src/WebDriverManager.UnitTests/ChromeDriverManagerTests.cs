ac07efc1b53de63cfd2669c13a785b3656212dce
Robert Barnes <robert.barnes@btl.com>
2019-11-11 12:18:26 +0000

    Add initial unit tests

---
+++ src/WebDriverManager.UnitTests/ChromeDriverManagerTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace WebDriverManager.UnitTests
+{
+    [TestFixture]
+    public class ChromeDriverManagerTests
+    {
+        [Test]
+        public void GetDriverManager()
+        {
+            WebDriverManager manager = WebDriverManager.chromedriver();
+
+            Assert.That(manager, Is.InstanceOf<ChromeDriverManager>());
+        }
+    }
+}
---
+++ src/WebDriverManager.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WebDriverManager.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WebDriverManager.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c078caf7-19c9-46f3-bdcc-90bf18d42f17")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
